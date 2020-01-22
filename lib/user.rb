ce19e8d94aa64cfed138974369c763ac7470dd32
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-22 01:46:36 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User
   attr_accessor :first_name, :last_name
 
   def initialize(first_name, last_name)
-    @first_name = "Tracy"
-    @last_name = "Morgan"
+    @first_name = "Avi"
+    @last_name = "Flombaum"
   end
 end
