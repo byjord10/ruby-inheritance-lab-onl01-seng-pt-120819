f553d9f928c337178b67cc30e20cae461ec00290
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-22 02:02:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
+class Student < User
+  attr_accessor :first_name, :last_name
+  @@knowledge = []
+
+  def initialize(first_name, last_name)
+    @first_name = "Steve"
+    @last_name = "Jobs"
+  end
 
 end
